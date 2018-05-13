b13ed9ecad013a2c7ffa7f5d9f4726a635ed2ce4
hey-yahei <31240114+hey-yahei@users.noreply.github.com>
2018-05-13 12:44:44 +0800

    移除多余的demos

--- code-editor/code.js
+++ code-editor/code.js
@@ -375,7 +375,7 @@ Code.init = function() {
            length: 3,
            colour: '#ccc',
            snap: true},
-       media: '../../media/',
+       media: '../media/',
        rtl: rtl,
        toolbox: toolboxXml,
        zoom:
@@ -507,6 +507,6 @@ Code.discard = function() {
 // Load the Code demo's language strings.
 document.write('<script src="msg/' + Code.LANG + '.js"></script>\n');
 // Load Blockly's language strings.
-document.write('<script src="../../msg/js/' + Code.LANG + '.js"></script>\n');
+document.write('<script src="../msg/js/' + Code.LANG + '.js"></script>\n');
 
 window.addEventListener('load', Code.init);
