886de41ae4c5c86b9cbaeea7e404d90cb9afa641
hey-yahei <31240114+hey-yahei@users.noreply.github.com>
2018-05-21 00:02:07 +0800

    重整项目目录

--- server.py
+++ server.py
@@ -3,31 +3,25 @@
 
 from bottle import route, static_file, run, template, request
 import subprocess
-from threading import Thread, Lock
 import platform
 import webbrowser
 
 coding = 'gbk' if platform.system() == "Windows" else 'utf-8'
 
-out_lock = Lock()
-p = None
-def run_thread():
-    os.system("python3 tmp/code2run.py")
-
 @route("/<filename:path>")
 def static_files(filename):
-    return static_file(filename, root="")
+    return static_file(filename, root="static/")
 
 @route("/")
 def index():
-    return template("index.html")
+    return template("static/index.html")
 
 @route("/do_run", method="post")
 def do_run():
     code = request.forms.code
     with open("tmp/code2run.py", "w") as f:
         f.write(code)
-        f.write("\nprint('python_all_ok')")
+        f.write("\nprint('----------- finished -----------')")
     p = subprocess.Popen('python tmp/code2run.py', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     p.wait()
 
