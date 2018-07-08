cc6522e91ecc04094c3bf6802a9ecfc5e1dfa7ba
hey-yahei <31240114+hey-yahei@users.noreply.github.com>
2018-07-08 10:35:11 +0800

    载入时检查是否有正在运行的程序
    页面首次加载时检查是否有正在运行的程序，有则加载模态框显示运行结果

--- server.py
+++ server.py
@@ -13,6 +13,7 @@ END_STRING = '----------- End -----------'
 
 coding = 'gbk' if platform.system() == "Windows" else 'utf-8'
 running_process = None
+all_result = ""
 
 if not os.path.exists("tmp"):
     print("create a new directory: tmp")
@@ -55,26 +56,37 @@ def download_python_file():
 
 @route("/get_running_state")
 def get_running_state():
-    global running_process
+    global running_process, all_result
 
     if running_process:
         result = running_process.stdout.readline().decode(coding)
+        all_result += result
         if running_process.poll() != None:
             result += running_process.stdout.read().decode(coding)
             result += "\n" + END_STRING
             running_process.terminate()
             running_process = None
+            all_result = ""
         return result
     else:
         return ""
 
 @route("/kill_program")
 def kill_program():
-    global running_process
+    global running_process, all_result
     if running_process:
         running_process.terminate()
         running_process = None
+        all_result = ""
     return "ok"
+
+@route("/load_states")
+def load_states():
+    global running_process, all_result
+    if running_process:
+        return all_result
+    else:
+        return ""
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- static/js/run.js
+++ static/js/run.js
@@ -2,6 +2,34 @@ var timer = null;
 var END_STRING = '----------- End -----------';  // 跟python中的定义统一
 var KILLED_STRING = '----------- Killed -----------';
 
+$(document).ready(function(){
+    $.get(
+        "/load_states",
+        function(result){
+            if(result != ""){
+                $(".modal").modal("show");
+                $("#run-result").text(result);
+                $("#modal-close").css("display", "none");
+                $("#killProgramButton").removeAttr("disabled");
+
+                timer = setInterval(function(){
+                    $.get(
+                        "/get_running_state",
+                        function(result){
+                            $("#run-result").append(result);
+                            if(result.indexOf(END_STRING) != -1){
+                                $("#modal-close").css("display", "inline");
+                                $("#killProgramButton").attr("disabled", "disabled");
+                                clearInterval(timer);
+                            }
+                        }
+                    )
+                }, 500);
+            }
+        }
+    )
+});
+
 // code.js中将run按钮的点击事件与Code.runPY绑定
 Code.runPY = function(){
     var code = Blockly.Python.workspaceToCode(Code.workspace);
@@ -30,7 +58,7 @@ Code.runPY = function(){
                             }
                         }
                     )
-                }, 500)
+                }, 500);
             }
         }
     );
